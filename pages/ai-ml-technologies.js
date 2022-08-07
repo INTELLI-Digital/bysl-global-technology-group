3cf43d22f1aeda7f2a36043b6eebf0651e66b14b
shovonBYSL <kawser.shovon@intelli.global>
2022-08-07 17:12:09 +0600

    AI-ML Banner updated

--- pages/ai-ml-technologies.js
+++ pages/ai-ml-technologies.js
@@ -1,5 +1,5 @@
 import BasicLayout from "../layouts/BasicLayout";
-import PrimaryBanner from "../components/shared/banners/PrimaryBanner";
+import AIMLBanner from "../components/technologies_pages/ai_ml_technologies/AIMLBanner";
 import Overview4Items from "../components/shared/Overview4Items";
 import BannerWithBulletSingle from "../components/shared/banners/BannerWithBulletSingle";
 import AIMLServices from "../components/technologies_pages/ai_ml_technologies/AIMLServices";
@@ -21,9 +21,9 @@ const AIMLTechnologies = ({
 }) => {
   return (
     // this component is wrapped in a layout which contains some of the common components in maximum pages
-    <BasicLayout title="AI/ML Technologies">
+    <BasicLayout title="AI/ML Technologies" noMargin={true}>
       {/* banner section  */}
-      <PrimaryBanner
+      <AIMLBanner
         title="AI/ML Technologies"
         sub="Innovating in next-generation AI systems development adapting data-driven supervised, unsupervised, reinforced learnings using self-replicating artificial neural networks & ML artifacts to handle & simplify the massive complexity of data patterns & open new horizons for businesses. "
         img={banner}
@@ -31,24 +31,26 @@ const AIMLTechnologies = ({
         Intelligent <TextGradient text=" Systems " />
         <br />
         to make <TextGradient text=" Life " /> Better
-      </PrimaryBanner>
+      </AIMLBanner>
 
-      {/* overview items section  */}
-      <Overview4Items data={aimlFeatures} />
+      <div className="box">
+        {/* overview items section  */}
+        <Overview4Items data={aimlFeatures} />
 
-      {/* services section  */}
-      <AIMLServices data={aimlServices} video={servicesVideo} />
+        {/* services section  */}
+        <AIMLServices data={aimlServices} video={servicesVideo} />
 
-      {/* Solutions section  */}
-      <ImageShowcase type="Applications" video={applicationsVideo}>
-        <TextGradient text="AI-driven " /> technology to unlock
-        <br />
-        the potential of
-        <TextGradient text=" health data" />
-      </ImageShowcase>
+        {/* Solutions section  */}
+        <ImageShowcase type="Applications" video={applicationsVideo}>
+          <TextGradient text="AI-driven " /> technology to unlock
+          <br />
+          the potential of
+          <TextGradient text=" health data" />
+        </ImageShowcase>
 
-      {/* Big Data Analysis section  */}
-      <BannerWithBulletSingle data={aimlUserExperience} type="Features" />
+        {/* Big Data Analysis section  */}
+        <BannerWithBulletSingle data={aimlUserExperience} type="Features" />
+      </div>
     </BasicLayout>
   );
 };
